843ec666ad26b6e605e6bee20d07be95958c9b30
Mike Hedman <mikehedman@yahoo.com>
2014-12-27 00:07:04 -0800

    Fixing path

--- ampersand-hoodie-model.js
+++ ampersand-hoodie-model.js
@@ -1,5 +1,5 @@
 var AmpersandModel = require('ampersand-model');
-var hoodieMixin = require('./ampersand-hoodie-mixin');
+var hoodieMixin = require('ampersand-hoodie-mixin');
 
 
 module.exports = AmpersandModel.extend(hoodieMixin, {
